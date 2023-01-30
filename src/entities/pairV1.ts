d34d839e9f2092266a7a9481ec5e432138b1fed8
gaepsuni <gaepsuni@joepegs.com>
2023-01-29 16:24:24 -0800

    check v1 factory in V1 swaps

--- src/entities/pairV1.ts
+++ src/entities/pairV1.ts
@@ -2,6 +2,7 @@ import { Address, log } from "@graphprotocol/graph-ts";
 import { Pair as PairV1 } from "../../generated/schema";
 import { Pair as PairContract } from "../../generated/Pair/Pair";
 import { loadToken } from "./token";
+import { V1_FACTORY_ADDRESS } from "../constants";
 
 export function loadV1Pair(address: Address): PairV1 | null {
   let v1Pair = PairV1.load(address.toHexString());
@@ -22,6 +23,19 @@ export function loadV1Pair(address: Address): PairV1 | null {
       log.info("[loadV1Pair] try_token1 reverted", []);
       return null;
     }
+
+    // check factory
+    const factory_addr = pair.try_factory()
+    if (token1Result.reverted) {
+      log.info("[loadV1Pair] try_factory reverted", []);
+      return null;
+    }
+
+    if (!factory_addr.value.equals(V1_FACTORY_ADDRESS)){
+      log.info("[loadV1Pair] wrong factory address", []);
+      return null;
+    }
+
 
     // create Tokens
     const token0 = loadToken(token0Result.value);
